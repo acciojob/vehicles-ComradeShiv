860acb16f2aa2f094cdf5c918765bc404045e73d
= <=>
2023-10-25 23:28:14 +0530

    Attempt 1

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,28 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, 2, 6, isManual, "Formula 1", 2);
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int currentSpeed = super.getCurrentSpeed();
+        super.setCurrentSpeed(currentSpeed + rate);
+        newSpeed = super.getCurrentSpeed();
+
+        if(newSpeed >= 0 && newSpeed <= 50) {
+            super.changeGear(1);
+        } else if(newSpeed >= 51 && newSpeed <= 100) {
+            super.changeGear(2);
+        } else if(newSpeed >= 101 && newSpeed <= 150) {
+            super.changeGear(3);
+        } else if(newSpeed >= 151 && newSpeed <= 200) {
+            super.changeGear(4);
+        } else if(newSpeed >= 201 && newSpeed <= 250) {
+            super.changeGear(5);
+        } else if(newSpeed >= 251){
+            super.changeGear(6);
+        }
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,11 +38,13 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            super.changeGear(1);
         }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+            changeSpeed(newSpeed, super.getCurrentDirection());
         }
     }
 }
